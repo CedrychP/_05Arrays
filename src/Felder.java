43d3e4d9aa20de01baaaee3f4a3c36c8100662d4
Philipp Cedrych <philipp.cedrych@compax.at>
2023-11-09 13:22:24 +0100

    Implementation

--- src/Felder.java
+++ src/Felder.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Felder {
+import java.util.Arrays;
+
+public class Felder{
+  public Felder(){
+
+  }
+  public char[] erzeugeCharFeld(char[] abc){
+
+    for(int i = 0; i< 26; i++){
+      abc[i]= (char) ('A' + i);
+    }
+
+    return abc;
+  }
+  public void arraysCopy(char[] abc){
+
+    char[] arraycopy = new char[26];
+    System.arraycopy(abc, 4, arraycopy, 4, 1);
+    System.out.println("Erster Copy Versuch mit System.arraycopy: " + arraycopy[4]);
+    arraycopy=Arrays.copyOf(abc, 4);
+    System.out.println("Zweiter Copy Versuch mit Arrays.copyOF: " + arraycopy[3]);
+  }
+
 }
